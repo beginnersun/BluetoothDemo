4ce57520b9d8cfa9c56139da83ae062c40f507f1
ThisMe <1985848358@qq.com>
2019-06-09 23:57:10 +0800

    新增连接功能

--- app/src/main/java/com/example/bluetoothdemo/activity/main/MainActivity.java
+++ app/src/main/java/com/example/bluetoothdemo/activity/main/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.bluetoothdemo.activity.main;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.le.BluetoothLeScanner;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,23 +11,27 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import io.reactivex.functions.Consumer;
 
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.bluetoothdemo.R;
 import com.example.bluetoothdemo.activity.main.adapter.BluetoothDeviceAdapter;
-import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.example.bluetoothdemo.base.BaseRecyclerViewAdapter;
+import com.example.bluetoothdemo.base.ViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -58,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView computerRecyclerView;
     private RecyclerView audioRecyclerView;
     private RecyclerView unknowRecyclerView;
+    private ImageView loadingView;
 
 
     private List<String> bluetoothType = new ArrayList<>();
@@ -65,43 +68,34 @@ public class MainActivity extends AppCompatActivity {
     private int scanTime = 10;  //10秒后 停止扫描设备
     private Handler handler = new Handler();
     private BluetoothReceiver bluetoothReceiver;
+
+    private BluetoothDevice connect_device;  //准备连接的设备
     /**
      * 是否正在扫描蓝牙设备
      */
     private boolean mScaning = false;
-    RxPermissions rxPermissions;
+//    RxPermissions rxPermissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rxPermissions = new RxPermissions(this);
+//        rxPermissions = new RxPermissions(this);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (!bluetoothAdapter.enable()) {
             Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(intent, REQUEST_BLUETOOTH);
         } else {
-            bluetoothAdapter.startLeScan(bluetoothScanCallback = new BluetoothScanCallback());  //仅能发现低功耗蓝牙(ble)
-            mScaning = true;
-            handler.postDelayed(stopScan, scanTime * 1000);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                Toast.makeText(this, "申请权限", Toast.LENGTH_SHORT).show();
-//                rxPermissions.request(Manifest.permission.ACCESS_COARSE_LOCATION).subscribe(new Consumer<Boolean>() {
-//                    @Override
-//                    public void accept(Boolean aBoolean) throws Exception {
-//                        Log.e(TAG, "accept: 定位权限"+aBoolean);
-//                        if (aBoolean){
-
-//                        }else {
-//                            Toast.makeText(MainActivity.this,"开启定位权限才可以扫描",Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                });
-            }
+            searchBleDevice();
         }
-
         initBluetoothDevices();
-//        searchJindDianDevice();
+    }
+
+    private void searchBleDevice(){
+        bluetoothAdapter.startLeScan(bluetoothScanCallback = new BluetoothScanCallback());  //仅能发现低功耗蓝牙(ble)
+        mScaning = true;
+        handler.postDelayed(stopScan, scanTime * 1000);
+        searchJindDianDevice();
     }
 
     private Runnable stopScan = new Runnable() {
@@ -115,15 +109,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void initBluetoothDevices() {
         phoneAdapter = new BluetoothDeviceAdapter(this, phoneDevices, BluetoothDeviceAdapter.DEVICE_PHONE);
-        miscAdapter = new BluetoothDeviceAdapter(this, miscDevices, BluetoothDeviceAdapter.DEVICE_PHONE);
-        computerAdapter = new BluetoothDeviceAdapter(this, computerDevices, BluetoothDeviceAdapter.DEVICE_PHONE);
-        audioAdapter = new BluetoothDeviceAdapter(this, audioDevices, BluetoothDeviceAdapter.DEVICE_PHONE);
-        unknowAdapter = new BluetoothDeviceAdapter(this, unknowDevices, BluetoothDeviceAdapter.DEVICE_PHONE);
+        miscAdapter = new BluetoothDeviceAdapter(this, miscDevices, BluetoothDeviceAdapter.DEVICE_BLUETOOTH);
+        computerAdapter = new BluetoothDeviceAdapter(this, computerDevices, BluetoothDeviceAdapter.DEVICE_COMPUTER);
+        audioAdapter = new BluetoothDeviceAdapter(this, audioDevices, BluetoothDeviceAdapter.DEVICE_AUDIO);
+        unknowAdapter = new BluetoothDeviceAdapter(this, unknowDevices, BluetoothDeviceAdapter.DEVICE_BLUETOOTH);
 
         computerRecyclerView = findViewById(R.id.computer_recyclerView);
         phoneRecyclerView = findViewById(R.id.phone_recyclerView);
         audioRecyclerView = findViewById(R.id.audio_recyclerView);
         unknowRecyclerView = findViewById(R.id.unknow_recyclerView);
+        loadingView = findViewById(R.id.loading_view);
 
         computerRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         phoneRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -134,6 +129,32 @@ public class MainActivity extends AppCompatActivity {
         phoneRecyclerView.setAdapter(phoneAdapter);
         audioRecyclerView.setAdapter(audioAdapter);
         unknowRecyclerView.setAdapter(unknowAdapter);
+
+        phoneAdapter.setOnItemClickListener(new BaseRecyclerViewAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                phoneAdapter.showItemOptions(position);
+//                device.createInsecureRfcommSocketToServiceRecord("uuid"); //传统蓝牙连接方式
+//                device.connectGatt();  //ble蓝牙连接方式
+//                device.createInsecureRfcommSocketToServiceRecord();
+//                device.createRfcommSocketToServiceRecord();
+//                device.createIn
+            }
+        });
+        phoneAdapter.setOnOptionsClick(new BluetoothDeviceAdapter.OnOptionsClick() {
+            @Override
+            public void clickOptions(View view) {
+                int position = (int) view.getTag();
+                switch (view.getId()){
+                    case R.id.connect:
+                        connect_device = phoneDevices.get(position);
+
+                        break;
+                    case R.id.send:
+                        break;
+                }
+            }
+        });
 
     }
 
@@ -156,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
             handler.removeCallbacks(stopScan); //移除消息
             bluetoothAdapter.stopLeScan(bluetoothScanCallback);
         }
-//        unregisterReceiver(bluetoothReceiver);
+        unregisterReceiver(bluetoothReceiver);
     }
 
     class BluetoothReceiver extends BroadcastReceiver {
@@ -266,22 +287,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_BLUETOOTH) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "开启蓝牙，开始扫描", Toast.LENGTH_SHORT).show();
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    Toast.makeText(this, "申请权限", Toast.LENGTH_SHORT).show();
-                    rxPermissions.request(Manifest.permission.ACCESS_COARSE_LOCATION).subscribe(new Consumer<Boolean>() {
-                        @Override
-                        public void accept(Boolean aBoolean) throws Exception {
-                            Log.e(TAG, "accept: 定位权限" + aBoolean);
-                            if (aBoolean) {
-                                bluetoothAdapter.startLeScan(bluetoothScanCallback = new BluetoothScanCallback());  //仅能发现低功耗蓝牙(ble)
-                                mScaning = true;
-                                handler.postDelayed(stopScan, scanTime * 1000);
-                            } else {
-                                Toast.makeText(MainActivity.this, "开启定位权限才可以扫描", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
+                searchBleDevice();
             } else {
                 Toast.makeText(this, "开启蓝牙失败", Toast.LENGTH_SHORT).show();
             }
